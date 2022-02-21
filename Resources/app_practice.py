c4c8145163b8a3775bff2522481d86b6a48abc1d
Pau Rodriguez <pau@apothca.com>
2022-02-20 22:17:04 -0800

    updated .gitignore

--- Resources/app_practice.py
+++
@@ -1,8 +0,0 @@
-# Create a New Python File and Import the Flask Dependency
-from flask import Flask
-# Create a New Flask App Instance
-app = Flask(__name__)
-# Create Flask Routes
-@app.route('/')
-def hello_name():
-    return " Hello world"
